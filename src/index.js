00bedf1109721eec01d985da6540173fc5e69073
Brandon Boatright <bboatright013@gmail.com>
2020-09-28 14:57:21 -0400

    ES2015 Modules Exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import fruits from './foods';
+// import {choice, remove} from "./helpers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//     let theseFruits = fruits.map();
+//     let ourchoice = choice(theseFruits);
+//     let randomfruit = theseFruits[ourchoice];
+//     console.log(`I\'d like one ${randomfruit} please`);
+//     console.log(`Here you go: ${randomfruit}`);
+//     let remaining = remove(theseFruits);
+//     console.log(`Delicious! Mat I has another ${randomfruit}?`);
+//     console.log(`I\'m sorry but were all out. we have  ${remaining} left`);
+
+
+import fruits from './foods';
+import {choice, remove} from './helpers';
+
+let fruit = choice(fruits);
+
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remaining = remove(fruit, fruits);
+console.log(remaining);
+
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
+
